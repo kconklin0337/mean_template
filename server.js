5c75eded7b6d2571e528272a9170b5e8eb796394
Kevin <kevinskyhighhh@hotmail.com>
2016-02-01 18:02:02 -0500

    Twitter replica

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express'),
     server = express();
 
+server.use(express.static(__dirname+"/css"));
+
 server.get('/', function(req, res){
-	res.sendFile('html/index.html', { root: __dirname });
+	   res.sendFile('html/index.html', { root: __dirname });
+});
+
+server.post('/signup', function (req, res){
+    console.log("Hazzah!")
+    // console.log(req.body); //
+    res.redirect('/');
 });
 
 server.listen(8080, function(){
-	console.log("Now listening on port 8080");
+	  console.log("Now listening on port 8080");
 });
